88f8ca650a34507b25adc21a3c12b75511fe3cfb
Mark Fontenot <mfonten@lyle.smu.edu>
2020-09-30 08:39:57 -0500

    sample code with memory leak

---
+++ tester.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char** argv){
+
+    cout << "Hello World!" << endl;
+    int x = 10;
+
+    int* ptr = new int[x];
+    ptr[0] = 25;
+    return 0;
+
+}
